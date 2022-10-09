567a4195fa88a49e92458a931bd062bfebc75de5
soffanderson <s.btudz@gmail.com>
2022-10-09 13:22:12 +0800

    Selesai useState dalam komponent

--- belajar-6-useState/src/App.jsx
+++ belajar-6-useState/src/App.jsx
@@ -1,29 +1,10 @@
 import { PlaceContentCenter } from './components/PlaceContentCenter';
-import Button from './components/Button';
-import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    function handlerClick() {
-        setCount((prev) => prev + 1);
-    }
-
     return (
         <PlaceContentCenter>
-            <h1 className='text-5xl font-bold'>{count}</h1>
-            <div className='mt-5 flex gap-4'>
-                <Button onClick={handlerClick}>Count +1</Button>
-                <Button
-                    onClick={() => {
-                        handlerClick();
-                        handlerClick();
-                        handlerClick();
-                    }}
-                >
-                    Count +3
-                </Button>
-            </div>
+            <Counter initialValue={10} />
         </PlaceContentCenter>
     );
 }
