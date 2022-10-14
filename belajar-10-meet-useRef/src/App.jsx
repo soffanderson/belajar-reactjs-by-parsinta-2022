31d8a42ec0caebd643798dad0b13d76c7f1a20fd
soffanderson <s.btudz@gmail.com>
2022-10-14 23:14:02 +0800

    Menggunakan forwardRef selesai

--- belajar-10-meet-useRef/src/App.jsx
+++ belajar-10-meet-useRef/src/App.jsx
@@ -1,14 +1,17 @@
 import { PlaceContentCenter } from './components/PlaceContentCenter';
-import { useState } from 'react';
-import Todo from './components/Todo';
+import { useRef, useState } from 'react';
 import Card from './components/Card';
 import Button from './components/Button';
 
 function App() {
-    const [tick, setTick] = useState(0);
+    // const [tick, setTick] = useState(0);
+    const inputRef = useRef(null);
+
     function hanldeClick() {
         // const nextTick = tick + 1;
-        setTick((nextTick) => nextTick + 1);
+        // setTick((nextTick) => nextTick + 1);
+        inputRef.current.focus();
+        console.log(inputRef);
     }
 
     return (
@@ -16,9 +19,10 @@ function App() {
             <Card>
                 <Card.Title>useRef Hooks</Card.Title>
                 <Card.Body>
+                    <input ref={inputRef} className='border border-slate-700 my-2' />
                     <Button onClick={hanldeClick}>Tick</Button>
                 </Card.Body>
-                <Card.Footer>You clicked {tick} times.</Card.Footer>
+                {/* <Card.Footer>You clicked {tick} times.</Card.Footer> */}
             </Card>
         </PlaceContentCenter>
     );
--- belajar-10-meet-useRef/src/components/Input.jsx
+++ belajar-10-meet-useRef/src/components/Input.jsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 
-export function Input({ type = 'text', ...props }) {
+const Input = React.forwardRef({ type = 'text', ...props }, ref) => {
     return (
         <input
+        ref={ref}
             {...props}
             className='focus:outline-none focus:ring focus:ring-blue-200 transition duration-200 border-slate-300 rounded-lg shadow-sm w-full'
             type={type}
         />
     );
-}
+};
+
+export default Input;
